6baeebe56eeed01c765cf7fd219790ab0a7e4697
Matteo Ginetti <matteo.ginetti@24consulting.it>
2020-10-06 16:58:20 +0200

    First commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,48 @@
 import Vue from "vue";
+import Vuex from "vuex";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+import "./assets/style.scss";
 
+Vue.use(Vuex);
 Vue.config.productionTip = false;
+console.log(process.env);
+const store = new Vuex.Store({
+  state: {
+    matches: [],
+  },
+  getters: {
+    getMatches: (state) => state.matches,
+  },
+  mutations: {
+    updateMatches: (state, item) => {
+      console.log(item);
+      state.matches = [...item];
+    },
+  },
+  actions: {
+    fetchMatch: ({ commit }, item = {}) => {
+      axios
+        .get(process.env.VUE_APP_NBA_GAMES, {
+          params: {
+            start_date: new Date().toLocaleDateString("en", {
+              year: "numeric",
+              month: "numeric",
+              day: "numeric",
+            }),
+            ...item,
+          },
+        })
+        .then((response) => {
+          commit("updateMatches", response.data.data);
+        });
+    },
+  },
+});
 
 new Vue({
   router,
-  render: h => h(App)
+  store,
+  render: (h) => h(App),
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,111 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="title">
+      <span>NEXT MATCH</span>
+    </div>
+    <div class="container">
+      <template v-for="match in matches">
+        <div :key="match.id" class="card">
+          <div class="team">
+            <div class="logo">{{ match.home_team.abbreviation }}</div>
+            <div class="name">{{ match.home_team.full_name }}</div>
+          </div>
+          <div class="info">
+            <span>VS</span>
+            <span class="hour">19:00</span>
+          </div>
+          <div class="team">
+            <div class="logo">{{ match.visitor_team.abbreviation }}</div>
+            <div class="name">
+              {{ match.visitor_team.full_name }}
+            </div>
+          </div>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  created() {
+    this.$store.dispatch("fetchMatch");
+  },
+  computed: {
+    matches() {
+      return this.$store.getters.getMatches;
+    }
   }
 };
 </script>
+
+<style lang="scss">
+.home {
+  background: blue;
+  min-height: 100vh;
+  position: relative;
+  .title {
+    width: 100%;
+    text-align: center;
+    padding: 1.2rem;
+    span {
+      color: white;
+      font-weight: bold;
+      font-size: 1.8rem;
+    }
+  }
+  .container {
+    position: relative;
+    padding: 2.4rem 1.9rem;
+    .card {
+      box-shadow: 0px 5px 5px 2px rgba(224, 224, 224, 0.25);
+      background: white;
+      border-radius: 10px;
+      padding: 1.5rem;
+      display: flex;
+      flex-direction: row;
+      .team {
+        width: 43%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .logo {
+          height: 3.5rem;
+          width: 3.5rem;
+          border-radius: 50%;
+          background: gainsboro;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        .name {
+          padding: 0.7rem 1rem;
+          font-size: 1rem;
+          text-align: center;
+          width: calc(100% - 0.5rem);
+          text-overflow: ellipsis;
+          overflow: hidden;
+          white-space: nowrap;
+        }
+      }
+      .info {
+        justify-content: center;
+        align-items: center;
+        width: 14%;
+        display: flex;
+        flex-direction: column;
+        span {
+          font-size: 1.8rem;
+        }
+        .hour {
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+}
+</style>
